846a8b1cafa3dc6d9744e88271ba253f30e28d39
kidneygood <2979564623@qq.com>
2023-01-25 22:32:13 +0800

    chassis_mode动态刷新完成测试

--- application/referee/referee.h
+++ application/referee/referee.h
@@ -5,6 +5,7 @@
 #include "robot_def.h"
 #pragma pack(1)
 
+//模式是否切换标志位，0为未切换，1为切换，static定义默认为0
 typedef struct
 {
 	uint32_t chassis_flag  : 1;
@@ -13,7 +14,6 @@ typedef struct
 	uint32_t friction_flag  : 1;
 	uint32_t Power_flag  : 1;
 	uint32_t end_angle_flag  : 1;
-
 } Referee_Interactive_Flag_t; 
 
 
@@ -32,10 +32,15 @@ typedef struct
 	loader_mode_e loader_mode;//单发...连发
 	Chassis_Power_Data_s Chassis_Power_Data;// 功率控制
 
+	chassis_mode_e chassis_last_mode;//底盘模式
+	gimbal_mode_e gimbal_last_mode;//云台模式
+	shoot_mode_e shoot_last_mode;//发射模式设置
+	friction_mode_e friction_last_mode;//摩擦轮关闭
+	lid_mode_e lid_last_mode;//弹舱盖打开
+
 } Referee_Interactive_info_t;
 
 #pragma pack()
-
 
 void Referee_Interactive_init(void);
 void Referee_Interactive_task(void);
